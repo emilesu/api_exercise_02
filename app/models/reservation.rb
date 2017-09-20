ec36a94f5768d3f660b66f04f04026fb6851c798
emilesu <emilesu@qq.com>
2017-09-21 02:42:57 +0800

    add api routes

---
+++ app/models/reservation.rb
@@ -0,0 +1,15 @@
+class Reservation < ApplicationRecord
+  validates_presence_of :train_id, :seat_number, :booking_code
+  validates_uniqueness_of :seat_number, :scope => :train_id
+
+  belongs_to :train
+
+
+  # 在每个座位(Reservation)新建的时候,随机产生一个booking_code的 订票号码.
+  before_validation :generate_booking_code, :on => :create
+
+  def generate_booking_code
+    self.booking_code = SecureRandom.uuid
+  end
+  
+end
