1d8f6a8fb413314b0cf9d62f33efc9d1cda0b605
emilesu <emilesu@qq.com>
2017-09-21 12:22:36 +0800

    add available_seats function

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,7 +2,19 @@ class Train < ApplicationRecord
   validates_presence_of :number
   has_many :reservations
 
+  # 产生所有位置从 1A~6C
+  # ["1A", "1B", "1C", "2A", "2B", "2C", "3A", "3B", "3C",
+  #  "4A", "4B", "4C", "5A", "5B", "5C", "6A", "6B", "6C"]
+  SEAT = begin
+    (1..6).to_a.map do |series|
+      ["A","B","C"].map do |letter|
+        "#{series}#{letter}"
+      end
+    end
+  end
+
   def available_seats
-    ["1A", "1B", "1C", "1D", "1F"]
+    # 所有 SEATS 扣掉已经订位的资料
+    return SEAT - self.reservations.pluck(:seat_number)     #pluck方法:摘取
   end
 end
